180ebbec04833030513f6f7515c636b9603cf9df
bjohnson-oa <benjamin2.johnson@loreal.com>
2025-05-11 22:24:00 +0200

    push files

--- modules/demo_ingestion/dags/file_to_bq_dag.py
+++ modules/demo_ingestion/dags/file_to_bq_dag.py
@@ -16,7 +16,7 @@ default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'email_on_failure': True,
-    'email': "benjamin99.johnson@gmail.com" #use groups within an organization
+    'email': "benjamin99.johnson@gmail.com", #use groups within an organization
     'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
