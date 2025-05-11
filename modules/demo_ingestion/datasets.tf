159a4938085438ffe9f596298b023cf7d0087803
bjohnson-oa <benjamin2.johnson@loreal.com>
2025-05-11 09:39:22 +0200

    fix names

--- modules/demo_ingestion/datasets.tf
+++ modules/demo_ingestion/datasets.tf
@@ -1,7 +1,7 @@
 resource "google_bigquery_dataset" "common_datasets" {
   for_each      = local.common_datasets
   location      = local.location
-  dataset_id    = "d_${local.project_name}_${each.key}_${lower(local.location)}_${lower(local.environment)}"
-  friendly_name = "d_${local.project_name}_${each.key}_${lower(local.location)}_${lower(local.environment)}"
+  dataset_id    = "d_${local.project_id}_${each.key}_${lower(local.location)}_${lower(local.env)}"
+  friendly_name = "d_${local.project_id}_${each.key}_${lower(local.location)}_${lower(local.env)}"
   description   = each.value
 }
