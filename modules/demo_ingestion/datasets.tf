7ea36de7d0b05c279fa167c4d879686dbb53c1c2
bjohnson-oa <benjamin2.johnson@loreal.com>
2025-05-11 09:44:31 +0200

    init : module deployment

--- modules/demo_ingestion/datasets.tf
+++ modules/demo_ingestion/datasets.tf
@@ -1,7 +1,7 @@
 resource "google_bigquery_dataset" "common_datasets" {
   for_each      = local.common_datasets
   location      = local.location
-  dataset_id    = "d_${local.project_id}_${each.key}_${lower(local.location)}_${lower(local.env)}"
-  friendly_name = "d_${local.project_id}_${each.key}_${lower(local.location)}_${lower(local.env)}"
+  dataset_id    = "d_${local.usecase}_${each.key}_${lower(local.location)}_${lower(local.env)}"
+  friendly_name = "d_${local.usecase}_${each.key}_${lower(local.location)}_${lower(local.env)}"
   description   = each.value
 }
